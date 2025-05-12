f6a2dd38d9044897252fb6faeacb367c0f8f4093
iliekrishna <iliekrishna11@gmail.com>
2025-05-11 21:48:42 -0300

    Design de Telas

---
+++ app/src/main/java/com/example/acordascan/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.acordascan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnContinuar;
+    EditText edtEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnContinuar = findViewById(R.id.btnContinuar);
+        edtEmail = findViewById(R.id.edtEmail);
+
+    }
+    public void proximo (View view){
+        Intent intent = new Intent(MainActivity.this, senhadef.class);
+        startActivity(intent);
+
+    }
+}
